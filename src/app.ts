6d0adbb44467008509768aa7f111615d5ce2e5ce
nikitachoudhary114 <nikitachoudhary364@gmail.com>
2025-02-20 19:58:07 +0530

    authentication

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors"; // Import cors
 import { json } from "body-parser";
 import authRoutes from "./routes/authRoutes";
 
 const app = express();
 
+app.use(cors()); // Use cors middleware here
 app.use(json());
 app.use("/api/auth", authRoutes);
 
