8657da097e649d1bff572cc334b7c52bd8a80af2
nikitachoudhary114 <nikitachoudhary364@gmail.com>
2025-02-26 18:20:54 +0530

    cors issues

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,17 @@ import blogRoutes from "./routes/blogRoutes";
 
 const app = express();
 
-app.use(cors()); // Use cors middleware here
+
+app.use(
+  cors({
+    origin: [
+      "https://arevei-lovat.vercel.app/",
+      "https://arevei-backend.vercel.app",
+    ], // Adjust as needed
+    credentials: true,
+  })
+);
+
 app.use(json());
 app.use("/api/auth", authRoutes);
 app.use("/api/blogs", blogRoutes);
